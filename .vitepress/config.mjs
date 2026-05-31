8ee8f3ba9b11dc9cd3eb4ce78b522d83514d2d04
ff98sha <ff98sha>
2026-05-31 16:43:51 +0800

    fix ci

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import {defineConfig} from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     srcDir: "docs",
-	base: '/SJTU-Wiki/'
+	base: '/SJTU-Wiki/',
     title: "SJTU Wiki",
     description: "Encyclopedia of SJTU",
 	head: [['link', { rel: 'icon', href: '/favicon.png' }]],
